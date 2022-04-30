2402312a2df94282e69a433e0c881304d0e7a2f4
Admin <ghoillus@gmail.com>
2022-04-30 23:36:59 +0300

    Тесты с SQL Создание таблицы, проверка добавлений и проверка дублей

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import sqlite3
-import functions
 
-# Создание SQL
-conn = sqlite3.connect(r'sql/repices.db')
-cur = conn.cursor()
-cur.execute("""CREATE TABLE IF NOT EXISTS repices(
-   userid INT PRIMARY KEY,
-   name TEXT,
-   item TEXT,
-   repices TEXT);
-""")
-conn.commit()
-# Запуск
-functions.start()
+# Создание Базы
+db = sqlite3.connect('repices.db')
+sql = db.cursor()
+sql.execute("""CREATE TABLE IF NOT EXISTS repices (
+    name_rep TEXT,
+    discript_rep TEXT,
+    all_rep TEXT
+    )""")
+db.commit()
+
+# Получение значений
+# Все переведено в нижний регист, для избежание дублей в таблице
+name = input('Название рецепта:\n').lower()
+discript = input('Описание рецепта:\n').lower()
+main_rep = input('Инструкция рецепта:\n').lower()
+
+# Проверка имени
+sql.execute(f"SELECT name_rep FROM repices WHERE name_rep = '{name}'")
+if sql.fetchone() is None:
+    # Если совпадений нет, добавление
+    sql.execute(f'INSERT INTO repices VALUES(?,?,?)',(name,discript,main_rep))
+    db.commit()
+    print("Рецепт добавлен!")
+else:
+    # Если есть совпадения
+    print('Такой рецепт уже есть!')
+    for value in db.execute('SELECT * FROM repices'):
+        print(value)
