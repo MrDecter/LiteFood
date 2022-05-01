1e9f74c315379ccaa59e8729723cbcde2674ed38
Admin <ghoillus@gmail.com>
2022-05-01 22:24:19 +0300

    Небольшое оформление, добавление логики. Подумать об нормальном отображении

--- main.py
+++ main.py
@@ -10,21 +10,43 @@ sql.execute("""CREATE TABLE IF NOT EXISTS repices (
     )""")
 db.commit()
 
-# Получение значений
-# Все переведено в нижний регист, для избежание дублей в таблице
-name = input('Название рецепта:\n').lower()
-discript = input('Описание рецепта:\n').lower()
-main_rep = input('Инструкция рецепта:\n').lower()
+# Функции
 
-# Проверка имени
-sql.execute(f"SELECT name_rep FROM repices WHERE name_rep = '{name}'")
-if sql.fetchone() is None:
-    # Если совпадений нет, добавление
-    sql.execute(f'INSERT INTO repices VALUES(?,?,?)',(name,discript,main_rep))
-    db.commit()
-    print("Рецепт добавлен!")
-else:
-    # Если есть совпадения
-    print('Такой рецепт уже есть!')
-    for value in db.execute('SELECT * FROM repices'):
-        print(value)
+def AddRep():
+    # Получение значений
+    # Все переведено в нижний регист, для избежание дублей в таблице
+    name = input('Название рецепта:\n').lower()
+    discript = input('Описание рецепта:\n').lower()
+    main_rep = input('Инструкция рецепта:\n').lower()
+
+    # Проверка имени
+    sql.execute(f"SELECT name_rep FROM repices WHERE name_rep = '{name}'")
+    if sql.fetchone() is None:
+        # Если совпадений нет, добавление
+        sql.execute(f'INSERT INTO repices VALUES(?,?,?)',(name,discript,main_rep))
+        db.commit()
+        print("Рецепт добавлен!")
+    else:
+        # Если есть совпадения
+        print('Такой рецепт уже есть!')
+        for value in db.execute('SELECT * FROM repices'):
+            print(value)
+
+def AllRep():
+    for i in sql.execute("SELECT * FROM repices"):
+        print(i)
+
+
+
+def LiteFood():
+    print('Добро пожаловать в LiteFood! \n 1. Добавить рецепт \n 2. Посмотреть рецепты')
+    manag = int(input())
+    if manag == 1:
+        AddRep()
+    else:
+        AllRep()
+
+
+
+# Запуск
+LiteFood()
